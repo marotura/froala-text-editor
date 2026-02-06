c6b174f7e79060d42a52dfdca0062d96ebc5ff54
ArmindaMarin <arminda_marin19@hotmail.com>
2026-02-06 14:20:08 +0100

    Changed pasting functionally & dark mode style

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -11,3 +11,146 @@ export default {
   props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
 }
 </script>
+<style scoped>
+.fr-box {
+    margin: 8px;
+}
+
+.fr-view br {
+    display: block;
+    margin-bottom: 0.5em;
+    content: "";
+}
+
+.fr-view blockquote,
+.fr-view dd,
+.fr-view dl,
+.fr-view figure,
+.fr-view h1,
+.fr-view h2,
+.fr-view h3,
+.fr-view h4,
+.fr-view h5,
+.fr-view h6,
+.fr-view p,
+.fr-view pre {
+    margin: revert;
+}
+
+.fr-box {
+    margin: 8px;
+}
+
+.fr-view h1 {
+    display: block !important;
+    color: #333;
+    font-size: clamp(2rem, 5vw, 3rem);
+    font-weight: bold;
+    line-height: 1.2;
+}
+
+.fr-dropdown-list h1 {
+    display: block !important;
+}
+
+.fr-view h2 {
+    display: block !important;
+    color: #333;
+    font-size: clamp(1.75rem, 4.5vw, 2.5rem);
+    font-weight: bold;
+    line-height: 1.3;
+}
+
+.fr-view h3 {
+    display: block !important;
+    color: #333;
+    font-size: clamp(1.5rem, 4vw, 2rem);
+    font-weight: bold;
+    line-height: 1.4;
+}
+
+.fr-view h4 {
+    display: block !important;
+    color: #333;
+    font-size: clamp(1.25rem, 3.5vw, 1.75rem);
+    font-weight: bold;
+    line-height: 1.5;
+}
+
+.fr-view h5 {
+    display: block !important;
+    color: #333;
+    font-size: clamp(1rem, 3vw, 1.5rem);
+    font-weight: bold;
+    line-height: 1.6;
+}
+
+.dark .fr-view {
+    color: #94a1b4; /* slate-300, for dark mode */
+    background-color: #1e293b;
+}
+
+.dark .fr-view p,
+.dark .fr-view h1,
+.dark .fr-view h2,
+.dark .fr-view h3,
+.dark .fr-view h4,
+.dark .fr-view h5 {
+    color: #94a1b4; /* slate-200, for dark mode */
+}
+
+.dark .fr-view a {
+    color: #60a5fa;
+    cursor: pointer;
+    text-decoration: underline;
+}
+
+.dark .fr-view a:visited {
+    color: #c084fc;
+}
+
+.dark .fr-view a:hover {
+    color: #f87171;
+}
+
+.fr-view a {
+    color: #0000ee;
+    cursor: pointer;
+    text-decoration: underline;
+}
+
+.fr-view a:visited {
+    color: #551a8b;
+}
+
+.fr-view a:hover {
+    color: #ff0000;
+}
+
+.fr-view ul,
+.fr-view ol {
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 40px;
+}
+
+.fr-view ul {
+    list-style: disc;
+}
+
+.fr-view ol {
+    list-style: decimal;
+}
+
+.fr-popup .fr-layer-content {
+    max-height: 65vh;
+    overflow-y: auto;
+}
+
+.fr-view p {
+    margin: 0 0 1em 0;
+    line-height: 1.6;
+}
+</style>
--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,201 +1,557 @@
 <template>
-  <DefaultField
-    :field="field"
-    :errors="errors"
-    :show-help-text="showHelpText"
-    :full-width-content="fullWidthContent"
-  >
-    <template #field>
-      <div id="app">
-        <froala id="edit" :tag="'textarea'" :config="config" v-model:value="value"></froala>
-      </div>
-    </template>
-  </DefaultField>
+    <DefaultField
+        :field="field"
+        :errors="errors"
+        :show-help-text="showHelpText"
+        :full-width-content="fullWidthContent"
+    >
+        <template #field>
+            <div id="app">
+                <froala
+                    id="edit"
+                    :tag="'textarea'"
+                    :config="config"
+                    v-model:value="value"
+                ></froala>
+            </div>
+        </template>
+    </DefaultField>
 </template>
 
 <script>
-import { FormField, HandlesValidationErrors } from 'laravel-nova';
-//Import Froala Editor 
-import 'froala-editor/js/plugins.pkgd.min.js';
+import FroalaEditor from "froala-editor";
+import { FormField, HandlesValidationErrors } from "laravel-nova";
+//Import Froala Editor
+import "froala-editor/js/plugins.pkgd.min.js";
 //Import third party plugins
-import 'froala-editor/js/third_party/embedly.min';
-import 'froala-editor/js/third_party/font_awesome.min';
-import 'froala-editor/js/third_party/image_tui.min';
+import "froala-editor/js/third_party/embedly.min";
+import "froala-editor/js/third_party/font_awesome.min";
+import "froala-editor/js/third_party/image_tui.min";
 // Import Froala Editor css files.
-import 'froala-editor/css/froala_editor.pkgd.min.css';
-import 'froala-editor/css/froala_style.min.css';
+import "froala-editor/css/froala_editor.pkgd.min.css";
+import "froala-editor/css/froala_style.min.css";
 
 export default {
-  mixins: [FormField, HandlesValidationErrors],
+    mixins: [FormField, HandlesValidationErrors],
 
-  props: ['resourceName', 'resourceId', 'field'],
+    props: ["resourceName", "resourceId", "field"],
 
-  methods: {
-    /*
-     * Set the initial, internal value for the field.
-     */
-    setInitialValue() {
-      this.value = this.field.value || ''
-      this.config = this.field.options || {
-        attribution: false,
-        key: this.field.apiKey,
-        charCounterCount: false,
-        listAdvancedTypes: false,
-        pastePlain: true,
-        spellcheck: false,
-        wordCounterCount: false,
+    methods: {
+        /*
+         * Set the initial, internal value for the field.
+         */
+        setInitialValue() {
+            this.value = this.field.value || "";
+            this.config = this.field.options || {
+                attribution: false,
+                key: this.field?.apiKey,
+                language: this.field?.language || "en",
+                charCounterCount: false,
+                wordCounterCount: true,
+                // spellcheck: true,
+                heightMin: 200,
+                paragraphFormatSelection: true,
+                paragraphDefaultSelection: "Normal",
+                multiLine: true,
+                enter: FroalaEditor.ENTER_P,
+                cleanReduntantStyleOnGet: true,
+                clearFormatOnEnterNewLine: false,
+                listAdvancedTypes: false,
+                linkConvertEmailAddress: true,
+                linkEditButtons: ["linkOpen", "linkEdit", "linkRemove"],
+                toolbarSticky: false,
+                wordPasteModal: false,
+                wordPasteKeepFormatting: false,
+                importFromWordEnableImportOnDrop: true,
+                // pastePlain: true,
 
-        toolbarButtons: {
-          moreText: {
-            buttons: ['bold', 'italic', 'underline'],
-            buttonsVisible: 3
-          },
-          moreParagraph: {
-            buttons: ['paragraphFormat', 'formatOL', 'formatUL'],
-            buttonsVisible: 3
-          },
-          moreRich: {
-            buttons: ['insertLink', 'insertTable', 'specialCharacters'],
-            buttonsVisible: 3
-          },
-          moreMisc: {
-            buttons: ['undo', 'redo', 'fullscreen', 'html', 'help'],
-            buttonsVisible: 5
-          }
+                htmlAllowedTags: [
+                    "p",
+                    "h1",
+                    "h2",
+                    "h3",
+                    "h4",
+                    "h5",
+                    "h6",
+                    "strong",
+                    "em",
+                    "u",
+                    "a",
+                    "ul",
+                    "ol",
+                    "li",
+                    "br",
+                    "hr",
+                    "table",
+                    "thead",
+                    "tbody",
+                    "tr",
+                    "th",
+                    "td",
+                    "img",
+                    "blockquote",
+                    "code",
+                    "cite",
+                    "link",
+                    "textarea",
+                ],
+
+                htmlAllowedAttrs: [
+                    "accesskey",
+                    "align",
+                    "alt",
+                    "autofocus",
+                    "aria-.*",
+                    "cellpadding",
+                    "cellspacing",
+                    "cite",
+                    "class",
+                    "cols",
+                    "colspan",
+                    "content",
+                    "contextmenu",
+                    "coords",
+                    "data",
+                    "data-.*",
+                    "datetime",
+                    "default",
+                    "dir",
+                    "dirname",
+                    "disabled",
+                    "download",
+                    "for",
+                    "headers",
+                    "height",
+                    "hidden",
+                    "high",
+                    "href",
+                    "hreflang",
+                    "icon",
+                    "id",
+                    "itemprop",
+                    "label",
+                    "lang",
+                    "language",
+                    "list",
+                    "low",
+                    "max",
+                    "maxlength",
+                    "media",
+                    "min",
+                    "name",
+                    "open",
+                    "optimum",
+                    "ping",
+                    "placeholder",
+                    "pubdate",
+                    "readonly",
+                    "rel",
+                    "reversed",
+                    "rows",
+                    "rowspan",
+                    "shape",
+                    "size",
+                    "sizes",
+                    "span",
+                    "src",
+                    "srcdoc",
+                    "srclang",
+                    "srcset",
+                    "summary",
+                    "spellcheck",
+                    "style",
+                    "tabindex",
+                    "target",
+                    "title",
+                    "type",
+                    "value",
+                    "valign",
+                    "width",
+                    "wrap",
+                ],
+
+                pasteDeniedTags: [
+                    "script",
+                    "style",
+                    "meta",
+                    "link",
+                    "iframe",
+                    "object",
+                    "embed",
+                    "input",
+                    "button",
+                    "select",
+                    "option",
+                    "textarea",
+                    "applet",
+                    "frame",
+                    "frameset",
+                    "noscript",
+                    //"a",
+                    "svg",
+                    "math",
+                    "form",
+                    "base",
+                    "body",
+                    "head",
+                    "video",
+                    "audio",
+                    "track",
+                    "source",
+                ],
+
+                pasteDeniedAttrs: [
+                    "accesskey",
+                    "accept",
+                    "accept-charset",
+                    "action",
+                    "allowfullscreen",
+                    "allowtransparency",
+                    "autofocus",
+                    "async",
+                    "autocomplete",
+                    "autoplay",
+                    "autosave",
+                    "background",
+                    "bgcolor",
+                    "border",
+                    "charset",
+                    "checked",
+                    "cite",
+                    "color",
+                    "class",
+                    "contenteditable",
+                    "controls",
+                    "defer",
+                    "draggable",
+                    "dropzone",
+                    "enctype",
+                    "form",
+                    "formaction",
+                    "frameborder",
+                    "http-equiv",
+                    "icon",
+                    "ismap",
+                    "keytype",
+                    "kind",
+                    "loop",
+                    "low",
+                    "max",
+                    "maxlength",
+                    "media",
+                    "min",
+                    "method",
+                    "mozallowfullscreen",
+                    "multiple",
+                    "muted",
+                    "novalidate",
+                    "pattern",
+                    "playsinline",
+                    "preload",
+                    "poster",
+                    "radiogroup",
+                    "required",
+                    "sandbox",
+                    "scope",
+                    "scoped",
+                    "seamless",
+                    "scrolling",
+                    "selected",
+                    "sizes",
+                    "start",
+                    "step",
+                    "style",
+                    "summary",
+                    "spellcheck",
+                    "tabindex",
+                    "type",
+                    "data",
+                    "data-.*",
+                    "data-pasted",
+                    "translate",
+                    "usemap",
+                    "value",
+                    "valign",
+                    "webkitallowfullscreen",
+                    "width",
+                    "wrap",
+                    "onload",
+                    "onerror",
+                    "onclick",
+                    "onmouseover",
+                ],
+
+                wordDeniedAttrs: [
+                    "accesskey",
+                    "accept",
+                    "accept-charset",
+                    "action",
+                    "allowfullscreen",
+                    "allowtransparency",
+                    "autofocus",
+                    "async",
+                    "autocomplete",
+                    "autoplay",
+                    "autosave",
+                    "background",
+                    "bgcolor",
+                    "border",
+                    "charset",
+                    "checked",
+                    "cite",
+                    "color",
+                    "class",
+                    "contenteditable",
+                    "controls",
+                    "defer",
+                    "draggable",
+                    "dropzone",
+                    "enctype",
+                    "form",
+                    "formaction",
+                    "frameborder",
+                    "http-equiv",
+                    "icon",
+                    "ismap",
+                    "keytype",
+                    "kind",
+                    "loop",
+                    "low",
+                    "max",
+                    "maxlength",
+                    "media",
+                    "min",
+                    "method",
+                    "mozallowfullscreen",
+                    "multiple",
+                    "muted",
+                    "novalidate",
+                    "pattern",
+                    "playsinline",
+                    "preload",
+                    "poster",
+                    "radiogroup",
+                    "required",
+                    "sandbox",
+                    "scope",
+                    "scoped",
+                    "seamless",
+                    "scrolling",
+                    "selected",
+                    "sizes",
+                    "start",
+                    "step",
+                    "style",
+                    "summary",
+                    "spellcheck",
+                    "tabindex",
+                    "type",
+                    "data",
+                    "data-.*",
+                    "data-pasted",
+                    "translate",
+                    "usemap",
+                    "value",
+                    "valign",
+                    "webkitallowfullscreen",
+                    "width",
+                    "wrap",
+                    "onmouseover",
+                ],
+
+                pasteAllowedStyleProps: [
+                    "font-size",
+                    "font-style", //italic, etc.
+                    "font-weight", //bold, etc.
+                    "text-decoration", //underline, line-through, etc.
+                    "text-transform", //Capitalize, uppercase, lowercase
+                    "text-indent", //indentation
+                    "text-align", //left, center, right, justify
+                ],
+
+                htmlDoNotWrapTags: ["br"],
+
+                quickInsertButtons: ["image", "table", "ul", "ol", "hr"],
+
+                toolbarButtons: {
+                    moreText: {
+                        buttons: ["bold", "italic", "underline"],
+                        buttonsVisible: 3,
+                    },
+                    moreParagraph: {
+                        buttons: ["paragraphFormat", "formatUL", "formatOL"],
+                        buttonsVisible: 3,
+                    },
+                    moreRich: {
+                        buttons: [
+                            "insertLink",
+                            "insertTable",
+                            "specialCharacters",
+                        ],
+                        buttonsVisible: 3,
+                    },
+                    moreMisc: {
+                        buttons: [
+                            "undo",
+                            "redo",
+                            "fullscreen",
+                            "html",
+                            "help",
+                        ],
+                        buttonsVisible: 5,
+                    },
+                },
+
+                toolbarButtonsXS: {
+                    moreText: {
+                        buttons: ["bold", "italic", "underline"],
+                        buttonsVisible: 2,
+                    },
+                    moreParagraph: {
+                        buttons: ["paragraphFormat", "formatOL", "formatUL"],
+                        buttonsVisible: 2,
+                    },
+                    moreRich: {
+                        buttons: ["insertLink", "insertTable"],
+                        buttonsVisible: 1,
+                    },
+                },
+
+                events: {
+                    initialized: function () {},
+                },
+            };
         },
 
-        toolbarButtonsXS: {
-          moreText: {
-            buttons: ['bold', 'italic', 'underline'],
-            buttonsVisible: 2
-          },
-          moreParagraph: {
-            buttons: ['paragraphFormat', 'formatOL', 'formatUL'],
-            buttonsVisible: 2
-          },
-          moreRich: {
-            buttons: ['insertLink', 'insertTable'],
-            buttonsVisible: 1
-          }
+        /**
+         * Fill the given FormData object with the field's internal value.
+         */
+        fill(formData) {
+            formData.append(this.fieldAttribute, this.value || "");
         },
-
-        events: {
-          initialized: function () {
-            
-          }
-        }
-      }
     },
-
-    /**
-     * Fill the given FormData object with the field's internal value.
-     */
-    fill(formData) {
-      formData.append(this.fieldAttribute, this.value || '')
-    },
-  },
-}
+};
 </script>
 <style>
+.fr-box {
+    margin: 8px;
+}
+
+.dark .fr-second-toolbar {
+    color: #cbd5e1; /* slate-300, for dark mode */
+    background-color: #0f172a;
+}
+
 .fr-view h1 {
-  display: block !important;
-  color: #333;
-  font-size: clamp(2rem, 5vw, 3rem);
-  font-weight: bold;
-  line-height: 1.2;
+    display: block !important;
+    color: #333;
+    font-size: clamp(2rem, 5vw, 3rem);
+    font-weight: bold;
+    line-height: 1.2;
 }
 
 .fr-dropdown-list h1 {
-  display: block !important;
+    display: block !important;
 }
 
 .fr-view h2 {
-  display: block !important;
-  color: #333;
-  font-size: clamp(1.75rem, 4.5vw, 2.5rem);
-  font-weight: bold;
-  line-height: 1.3;
+    display: block !important;
+    color: #333;
+    font-size: clamp(1.75rem, 4.5vw, 2.5rem);
+    font-weight: bold;
+    line-height: 1.3;
 }
 
 .fr-view h3 {
-  display: block !important;
-  color: #333;
-  font-size: clamp(1.5rem, 4vw, 2rem);
-  font-weight: bold;
-  line-height: 1.4;
+    display: block !important;
+    color: #333;
+    font-size: clamp(1.5rem, 4vw, 2rem);
+    font-weight: bold;
+    line-height: 1.4;
 }
 
 .fr-view h4 {
-  display: block !important;
-  color: #333;
-  font-size: clamp(1.25rem, 3.5vw, 1.75rem);
-  font-weight: bold;
-  line-height: 1.5;
+    display: block !important;
+    color: #333;
+    font-size: clamp(1.25rem, 3.5vw, 1.75rem);
+    font-weight: bold;
+    line-height: 1.5;
 }
 
 .fr-view h5 {
-  display: block !important;
-  color: #333;
-  font-size: clamp(1rem, 3vw, 1.5rem);
-  font-weight: bold;
-  line-height: 1.6;
+    display: block !important;
+    color: #333;
+    font-size: clamp(1rem, 3vw, 1.5rem);
+    font-weight: bold;
+    line-height: 1.6;
 }
 
+.dark .fr-view {
+    color: #cbd5e1; /* slate-300, for dark mode */
+    background-color: #0f172a;
+}
+
+.dark .fr-view p,
 .dark .fr-view h1,
 .dark .fr-view h2,
 .dark .fr-view h3,
 .dark .fr-view h4,
 .dark .fr-view h5 {
-  color: #e2e8f0; /* slate-200, for dark mode */
+    color: #94a1b4; /* slate-200, for dark mode */
 }
 
 .dark .fr-view a {
-  color: #60a5fa;
-  cursor: pointer;
-  text-decoration: underline;
+    color: #60a5fa;
+    cursor: pointer;
+    text-decoration: underline;
 }
 
 .dark .fr-view a:visited {
-  color: #c084fc;
+    color: #c084fc;
 }
 
 .dark .fr-view a:hover {
-  color: #f87171;
+    color: #f87171;
 }
 
 .fr-view a {
-  color: #0000EE;
-  cursor: pointer;
-  text-decoration: underline;
+    color: #0000ee;
+    cursor: pointer;
+    text-decoration: underline;
 }
 
 .fr-view a:visited {
-  color: #551A8B;
+    color: #551a8b;
 }
 
 .fr-view a:hover {
-  color: #FF0000;
+    color: #ff0000;
 }
 
 .fr-view ul,
 .fr-view ol {
-  margin-block-start: 1em;
-  margin-block-end: 1em;
-  margin-inline-start: 0px;
-  margin-inline-end: 0px;
-  padding-inline-start: 40px;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 40px;
 }
 
 .fr-view ul {
-  list-style: disc;
+    list-style: disc;
 }
 
 .fr-view ol {
-  list-style: decimal;
+    list-style: decimal;
 }
 
 .fr-popup .fr-layer-content {
-  max-height: 65vh;
-  overflow-y: auto;
+    max-height: 65vh;
+    overflow-y: auto;
+}
+
+.fr-view p {
+    margin: 0 0 1em 0;
+    line-height: 1.6;
 }
 </style>
